51716813a18bc66f57d718594642138bca339313
Anshoo <51527147+Anshoo1992@users.noreply.github.com>
2019-09-26 23:41:08 +0530

    Update user.js

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = new express.Router();
-const UsersModule = require('../constrollers/user');
+const UsersModule = require('../controllers/user');
 const authMiddleWare = require('../middleware/auth');
 const multer = require('multer');
 const upload = multer({
